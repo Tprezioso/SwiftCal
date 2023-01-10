4f8847bb9152bb9402e85429d9ce4d6cd7076760
Thomas <tommyprezioso@gmail.com>
2023-01-09 19:13:20 -0500

    Initial Commit

---
+++ SwiftCal/SwiftCalApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftCalApp.swift
+//  SwiftCal
+//
+//  Created by Thomas Prezioso Jr on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
